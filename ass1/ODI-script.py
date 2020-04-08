65f712b9c58239aee26e98c7ca65712efd457bf1
Jonathan <jmitnik@gmail.com>
2020-04-08 12:54:44 +0200

    Add additional classication code

--- ass1/ODI-script.py
+++ ass1/ODI-script.py
@@ -4,11 +4,12 @@ import pandas as pd
 from scipy.sparse import find
 from sklearn.compose import make_column_selector
 from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.compose import make_column_transformer
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import OneHotEncoder
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score, train_test_split
 import nltk
 from config import Config
 
@@ -31,22 +32,32 @@ df = pd.read_csv("data/ODI-2020.csv", sep=";", encoding="utf-8")
 df = transform_ODI_dataset(df)
 
 #%% Part 2 of preprocessing: Make it ready for ML algorithms
-inp = df.copy() # Copy as we will mutate the original dataframe otherwise
-target = inp.pop('programme') #Pop removes programme from
+working_data = df.copy() # Copy as we will mutate the original dataframe otherwise
+target = working_data.pop('programme') #Pop removes programme from
 
 # Creates a pipeline for the input which will transform our input to ndarrays
 preprocessing_pipeline_ODI = make_ODI_preprocess_pipeline(
-    inp,
+    working_data,
     config.min_word_count
 )
-
 target_encoder, target = preprocess_target(target)
 
-# Add to the preprocessing step a classification, and make this our final pipeline
-svc_model = SVC()
-classifier_svc = make_pipeline(preprocessing_pipeline_ODI, model)
+X_train, X_test, y_train, y_test = train_test_split(working_data, target, test_size=0.2)
 
-# Apply cross-validation to apply classifier to input and target
-performances = cross_val_score(classifier, inp, target)
+# Create a few classification pipelines for comparisons
+classifier_svc = make_pipeline(preprocessing_pipeline_ODI, SVC())
+classifier_tree = make_pipeline(preprocessing_pipeline_ODI, DecisionTreeClassifier())
 
+# Apply cross-validation to get representation of score
+svc_performance = cross_val_score(classifier_svc, X_train, y_train).mean()
+tree_performance = cross_val_score(classifier_tree, X_train, y_train).mean()
+
+# Now actually do training on complete training data
+classifier_svc.fit(X_train, y_train)
+classifier_tree.fit(X_train, y_train)
+
+# %%
+# Check feature importances of decision tree
+classifier_tree.named_steps.columntransformer.transformers[-1][1].get_feature_names()
+# classifier_tree.named_steps.decisiontreeclassifier.n_features_
 # %%
