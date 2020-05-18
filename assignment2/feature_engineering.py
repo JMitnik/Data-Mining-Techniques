49497211b0abb08371baf5bdda94a65cf8d58538
ruimtekapper <45589399+ruimtekapper@users.noreply.github.com>
2020-05-18 04:43:56 +0200

    Update and rename feature_engineering_draft.py to feature_engineering.py

--- assignment2/feature_engineering.py
+++ assignment2/feature_engineering.py
@@ -1,30 +1,33 @@
-#imports
+# Imports
+import sklearn as sk
+import pandas as pd
+from sklearn.impute import SimpleImputer
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import MinMaxScaler, OneHotEncoder,StandardScaler
+from sklearn.model_selection import train_test_split
+import lightgbm as lgb
 import numpy as np
 
+train_data = pd.read_csv('data/training_set_VU_DM.csv',nrows=1000
+                        )
 
+original_columns = train_data.columns
 
+time = pd.to_datetime(train_data['date_time'])
+
+train_data['month']=time.dt.month
+train_data['year']=time.dt.year
 train_data['same_country_visitor_prop']=np.where(train_data['visitor_location_country_id'] == train_data['prop_country_id'],1,0)
-
-#competitor combine nog niet aan de praat gekregen
-ccol=train_data.loc[:,['comp1_rate',
-                       'comp2_rate',
-                       'comp3_rate',
-                       'comp4_rate',
-                       'comp5_rate',
-                       'comp6_rate',
-                       'comp7_rate',
-                       'comp8_rate']]
-icol=train_data.loc[:,['comp1_inv',
-                       'comp2_inv',
-                       'comp3_inv',
-                       'comp4_inv',
-                       'comp5_inv',
-                       'comp6_inv',
-                       'comp7_inv',
-                       'comp8_inv']]
-
-train_data['viable_comp'] = np.where(ccol== -1 & icol == 0,1,0)
-
+train_data['viable_comp']= np.where(
+                  (train_data['comp1_rate']== -1)& (train_data['comp1_inv']== 0) |
+                  (train_data['comp2_rate']== -1)& (train_data['comp2_inv']== 0) |
+                  (train_data['comp3_rate']== -1)& (train_data['comp3_inv']== 0) |
+                  (train_data['comp4_rate']== -1)& (train_data['comp4_inv']== 0) |
+                  (train_data['comp5_rate']== -1)& (train_data['comp5_inv']== 0) |
+                  (train_data['comp6_rate']== -1)& (train_data['comp6_inv']== 0) |
+                  (train_data['comp7_rate']== -1)& (train_data['comp7_inv']== 0) |
+                  (train_data['comp8_rate']== -1)& (train_data['comp8_inv']== 0) 
+                  ,1,0)
 #Onehot encode the categorical variables
 oh_encoder = OneHotEncoder()
 oh_columns = ['site_id', 'visitor_location_country_id', 'prop_country_id', 'viable_comp', 'same_country_visitor_prop','prop_id', 'prop_brand_bool', 'promotion_flag', 
@@ -49,7 +52,7 @@ num_scale_columns = ['visitor_hist_starrating', 'visitor_hist_adr_usd',
                      'srch_length_of_stay', 'srch_booking_window', 
                      'srch_adults_count', 'srch_children_count',
                      'srch_room_count', 'srch_query_affinity_score', 
-                     'orig_destination_distance' 
+                     'orig_destination_distance','year','month'
                     ]
 #we do a preselection of columns that we feel will become useful features after encoding
 chosen_columns = ['prop_starrating', 'prop_review_score', 'prop_mean_score', 'viable_comp','same_country_visitor_prop' 
